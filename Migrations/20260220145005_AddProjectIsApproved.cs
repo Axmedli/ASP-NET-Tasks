7f4400f92e3cd00ce155debdefd3225786c17f08
Murad Ahmedli <ahmedli.murad06@bk.ru>
2026-02-20 19:33:21 +0400

    a

---
+++ Migrations/20260220145005_AddProjectIsApproved.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASP_NET_20._TaskFlow_FIle_attachment.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectIsApproved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "Projects",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "Projects");
+        }
+    }
+}
---
+++ Services/IProjectService.cs
@@ -0,0 +1,20 @@
+﻿using ASP_NET_20._TaskFlow_FIle_attachment.Models;
+using ASP_NET_20._TaskFlow_FIle_attachment.DTOs;
+
+namespace ASP_NET_20._TaskFlow_FIle_attachment.Services;
+
+public interface IProjectService
+{
+    Task<IEnumerable<ProjectResponseDto>> GetAllForUserAsync(string userId, IList<string> roles);
+    Task<ProjectResponseDto?> GetByIdAsync(int id);
+    Task<Project?> GetProjectEntityAsync(int id, bool includeUnapproved = false);
+    Task<ProjectResponseDto> CreateAsync(CreateProjectDto  createProjectDto, string ownerId);
+    Task<ProjectResponseDto?> UpdateAsync(int id, UpdateProjectDto  updateProjectDto);
+    Task<bool> DeleteAsync(int id);
+    Task<IEnumerable<ProjectMemberResponseDto>> GetMembersAsync(int projectId);
+    Task<IEnumerable<AvailableUserDto>> GetAvailableUsersToAddAsync(int projectId);
+    Task<bool> AddMemberAsync(int projectId, string userIdOrEmail);
+    Task<bool> RemoveMemberAsync(int projectId, string userId);
+    Task<IEnumerable<ProjectResponseDto>> GetPendingProjectsAsync();
+    Task<bool> ApproveAsync(int id);
+}
